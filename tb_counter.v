67524c6a0ca67dc7821b5c0da2de0c4ad9087ac8
Manoz790 <95535404+Manoz790@users.noreply.github.com>
2023-07-25 11:23:52 +0530

    Add files via upload

--- tb_counter.v
+++ tb_counter.v
@@ -3,7 +3,7 @@
 module tb_counter();
 reg clk,rst;
 wire q2,q1,q0;
-counter_3bitJK uut(clk,rst,q2,q1,q0);
+counter_3bit_using_TFF uut(clk,rst,q2,q1,q0);
 initial begin
  clk=0; rst= 1;
  #10; rst =0;
